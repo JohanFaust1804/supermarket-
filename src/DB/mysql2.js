5b84b91ac7312ba8272911d446e5259b254ba606
JohanFaust1804 <johan.jb137@gmail.com>
2024-11-25 14:49:54 -0500

    Update mysql2.js

--- src/DB/mysql2.js
+++ src/DB/mysql2.js
@@ -1,11 +1,35 @@
 const mysql2 = require('mysql2');
 const config = require('../config');
 
-const prueba = {
-    id : 1,
-    nombre: 'Juan',
-    apellido: 'Perez',
-    edad: 30
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
+}
+
+let conexion;
+
+function conMysql() {
+    conexion = mysql2.createConnection(dbconfig);
+
+    conexion.connect((err) => {
+        if (err){
+            console.log(['db err'], err);
+            setTimeout(conMysql, 200)
+        } else {
+            console.log('db conectada!!!');
+        }
+    });
+
+    conexion.on('error', err => {
+        console.log(['db err'], err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            conMysql();
+        } else{
+            throw err; 
+        }
+    });
 }
 
 function todos (tabla){
