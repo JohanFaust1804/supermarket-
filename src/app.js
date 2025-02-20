d2a1bb5db6264434842c4045b324fa26c0e68344
JohanFaust1804 <johan.jb137@gmail.com>
2025-02-20 13:51:18 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
         const express = require("express");
         const morgan = require('morgan');
+        const cors = require('cors');
         const config = require('./config');
 
 
         const clientes = require('./modulos/clientes/rutas');
+        const usuarios = require('./modulos/usuarios/rutas');
+        const error = require('./red/errors');
 
         const app = express();
 
+        // CORS
+        var corsOptions = {
+                origin: '*',
+                optionsSuccessStatus: 200
+    
+        }
+
         //Midleware
+        app.use(morgan('dev'));
+        app.use(cors(corsOptions));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
         app.use(morgan('dev'));
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
@@ -19,5 +33,7 @@
 
         //rutas
         app.use('/api/clientes', clientes);
+        app.use('/api/usuarios', usuarios);
+        app.use(error);
 
         module.exports = app;
